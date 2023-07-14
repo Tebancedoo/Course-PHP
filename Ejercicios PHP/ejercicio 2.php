db9a3ee184a134de2e0a4d0fc138e5433ae942bc
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-07-14 15:55:10 -0500

    Add files via upload

---
+++ Ejercicios PHP/ejercicio 2.php
@@ -0,0 +1,37 @@
+<?php
+
+    if ($_POST){ // Utilizo el condicional if para evitar el error de "array key undefined"
+
+        $nombre = $_POST["txtNombre"];// Defino la variable para que reciba la información del formulario
+
+        echo "Hola ".$nombre; //Concateno el valor de la variable con "hola"
+    
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2 PHP</title>
+</head>
+<body>
+    
+    <!--Creo un formulario basico-->
+    <form action="ejercicio 2.php" method="post">
+    Nombre: 
+
+    <input type="text" name="txtNombre" id="">
+
+    <br>
+
+    <input type="submit" value="Enviar">
+
+    </form>
+
+</body>
+</html>
