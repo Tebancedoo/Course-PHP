a73b6545bac8cc086f8ff257055a4aafd7c844e6
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-07-14 15:41:35 -0500

    Add files via upload

---
+++ Ejercicios PHP/ejercicio 1.php
@@ -0,0 +1,20 @@
+<!Doctype html>
+<html>
+    <head>
+        <utf8> 
+        <title>Ejercicio 1 PHP</title>
+    </head>
+    <body>
+        <?php
+
+            // varias formas de imprimir un mensaje
+            echo "Hola mundo con echo";
+
+            print_r("Hola mundo con print_r");
+
+            print("Hola mundo con print");
+
+            
+        ?>
+    </body>
+</html>
