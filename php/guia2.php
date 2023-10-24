03be97f3f9ed5b4f5c08b025e0adabf19026bba1
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-10-24 12:35:22 -0500

    Add files via upload

--- php/guia2.php
+++ php/guia2.php
@@ -14,7 +14,25 @@
 
     <?php
 
+    //defino las variables a usar
+    $color1 = "";
+    $color2 = "";
+    $color3 = "";
+    $color4 = "";
+
+    $significado1 = "";
+    $significado2 = "";
+    $significado3 = "";
+    $significado4 = "";
+
+    //defino los arrays
+    $arreglo = [ $color1  => "Amarrillo", $color2 => "Azul", $color3 => "Verde", $color4 => "Negro"];
+
+    $arreglo2 = [ $significado1 => "Alegría, originalidad e inteligencia", $significado2 => "", $significado3 => "", $significado4 => "Elegancia y poder", ];
+
+
     echo "<thead>";
+
     echo "<tr>";
     echo "<th> Nombre </th>";
     echo "<th> Dirrección</th>";
@@ -23,20 +41,21 @@
     echo "<th> Color favorito</th>";
     echo "<th> Significado</th>";
     echo "</tr>";
+
     echo "</thead>";
 
     echo "<tbody>";
 
     echo "<tr><td> Pedro gonzales </td> <td>Calle 155  </td> <td> 555 </td> <td>22/enero </td> <td>Amarrillo </td> <td>Calor </td> </tr>";
     echo "<tr><td> Laura Herrera </td> <td>Calle 160  </td> <td> 888 </td> <td>10/diciembre </td> <td>Azul </td> <td>Frio </td> </tr>";
-    echo "<tr><td> Luis Rodriguez </td> <td>Calle 789  </td> <td> 333 </td> <td>15/septiembre </td> <td>Verde </td> <td> Natrraleza </td> </tr>";
+    echo "<tr><td> Luis Rodriguez </td> <td>Calle 789  </td> <td> 333 </td> <td>15/septiembre </td> <td>Verde </td> <td> Naturaleza </td> </tr>";
 
     echo "</tbody>";
 
     ?>
 
     </table>
-
+                                                                  
 
 
 </body>
