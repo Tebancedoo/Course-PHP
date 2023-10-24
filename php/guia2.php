1e91be1a10a9154b003dd56756bef281c51d6fca
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-10-24 10:38:15 -0500

    Add files via upload

---
+++ php/guia2.php
@@ -0,0 +1,37 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guia 2 PHP</title>
+</head>
+<body>
+
+    <table border="3px" align="center">
+
+    <?php
+
+    echo "<td> Nombre </td>";
+
+    echo "<td> Dirrección</td>";
+
+    echo "<td> Telefono</td>";
+
+    echo "<td> Fecha cumpleaños</td>";
+
+    echo "<td> Color favorito</td>";
+
+    echo "<td> Significado</td>";
+
+    ?>
+
+    </table>
+
+
+
+
+</body>
+</html>
